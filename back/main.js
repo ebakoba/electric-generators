470f3d81d6bfa81c1266ea8ed7008329fc67d000
ebakoba <kaarelallemann@gmail.com>
2017-06-15 15:25:22 +0300

    live price update

--- back/main.js
+++ back/main.js
@@ -2,29 +2,30 @@
 var nordpool = require('nordpool')
 var prices = new nordpool.Prices()
 
-var app = require('express').createServer();
-var io = require('socket.io')(app);
+var http = require('http');
+var express = require('express'),
+    app = module.exports.app = express();
 
-
-app.listen(80);
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+server.listen(80);
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.send('Hello, world!');
 });
 
-io.on('connection', function (socket) {
-    setInterval(() => {
-        getLatestMarketValue((latestMarketValue) => {
-            socket.emit('currentPriceUpdate', { price: latestMarketValue });
-        });
-    }, 2000);
-});
+
+setInterval(() => {
+    getLatestMarketValue((latestMarketValue) => {
+        io.sockets.emit('currentPriceUpdate', { price: latestMarketValue });
+    });
+}, 2000);
 
 
 var getLatestMarketValue = (callback) => {
   prices.hourly({currency: 'EUR'}, (error, results) => {
     if(error) console.log(error);
-
+    
     var estonianResults = results.filter((result) => {
       if(result.area === "EE"){
         return result;
--- front/js/index.js
+++ front/js/index.js
@@ -4,3 +4,10 @@ Array.from(document.getElementsByClassName('generator-switch')).forEach((generat
         event.preventDefault();
     };
 });
+
+
+var socket = io.connect('http://localhost');
+
+socket.on('currentPriceUpdate', function (data) {
+    document.getElementById('livePrice').innerText = data.price + ' €/MWh';
+});
